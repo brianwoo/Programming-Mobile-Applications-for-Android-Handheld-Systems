53b3cc7c00bc661ce3f55f99df402574a7412873
Brian Woo <woo.brian@gmail.com>
2015-01-05 17:42:02 -0700

    Added Notification

--- Week8/DailySelfie/DailySelfie/src/com/bwoo/dailyselfie/SelfieAlarmBroadcastReceiver.java
+++ Week8/DailySelfie/DailySelfie/src/com/bwoo/dailyselfie/SelfieAlarmBroadcastReceiver.java
@@ -4,10 +4,13 @@
 package com.bwoo.dailyselfie;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.SystemClock;
 
 /**
@@ -17,7 +20,9 @@ import android.os.SystemClock;
 public class SelfieAlarmBroadcastReceiver extends BroadcastReceiver
 {
 	
-
+	// Notification ID to allow for future updates
+	private static final int MY_NOTIFICATION_ID = 1;
+	
 	public void setupAlarm(Context context)
 	{
 		
@@ -35,6 +40,34 @@ public class SelfieAlarmBroadcastReceiver extends BroadcastReceiver
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
+		
 		System.out.println("#### alarm received!!");
+		
+		Intent mNotificationIntent = new Intent(context,
+				MainActivity.class);
+		
+		PendingIntent mContentIntent = PendingIntent.getActivity(context, 0, 
+				mNotificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
+		
+		long[] mVibratePattern = { 0, 200, 200, 300 };
+		
+		// Define the Notification's expanded message and Intent:
+		Notification.Builder notificationBuilder = new Notification.Builder(
+				context)
+				.setTicker("Time for another selfie")
+				.setSmallIcon(android.R.drawable.stat_sys_warning)
+				.setAutoCancel(true)
+				.setContentTitle("Daily Selfie")
+				.setContentText(
+						"Time for another selfie 2")
+				.setContentIntent(mContentIntent)
+				.setVibrate(mVibratePattern);
+
+		// Pass the Notification to the NotificationManager:
+		NotificationManager mNotificationManager = 
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(MY_NOTIFICATION_ID,
+				notificationBuilder.build());
 	}
+
 }
